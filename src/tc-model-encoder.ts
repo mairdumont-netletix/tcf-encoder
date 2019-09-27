524f911a9de4927f84686664c6ed026b0333186b
Bartelt Denny <denny.bartelt@mairdumont.com>
2019-09-27 16:27:28 +0200

    fix: CoreSegment does not have SegmentType in first 3 bits

--- src/tc-model-encoder.ts
+++ src/tc-model-encoder.ts
@@ -27,18 +27,24 @@ export class TCModelEncoder implements Encoder<TCModel> {
     const numberEncoder = new NumberEncoder();
 
     const segments: string[] = value.split('.');
-    for (let i = 0; i < segments.length; i++) {
-      const segment: string = segments[i];
-      // first char will contain 6 bits, we only need the first 3
-      const segTypeBits: string = bitFieldEncoder.decode(segment.charAt(0));
-      const segType: number = numberEncoder.decode(segTypeBits.substr(0, 3));
 
-      const segmentEncoder = segmentEncoderLookup(segType);
-      if (segmentEncoder) {
-        tcModel = segmentEncoder.decode(segment, tcModel);
+    if (segments.length) {
+      // decode segment 0
+      tcModel = segmentEncoderLookup(SegmentType.CORE)!.decode(segments[0], tcModel);
+      // decode segment 1 - 4 if available
+      for (let i = 1; i < segments.length; i++) {
+        const segment: string = segments[i];
+        // first char will contain 6 bits, we only need the first 3
+        const segTypeBits: string = bitFieldEncoder.decode(segment.charAt(0));
+        const segType: number = numberEncoder.decode(segTypeBits.substr(0, 3));
+
+        const segmentEncoder = segmentEncoderLookup(segType);
+        if (segmentEncoder) {
+          tcModel = segmentEncoder.decode(segment, tcModel);
+        }
+
+        // console.log(`value=${value}, i=${i}, segment=${segment}, segTypeBits=${segTypeBits}, segType=${segType}`);
       }
-
-      // console.log(`value=${value}, i=${i}, segment=${segment}, segTypeBits=${segTypeBits}, segType=${segType}`);
     }
 
     return tcModel;
