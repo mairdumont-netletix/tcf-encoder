a548af7395186263814352af31d6bd9fea37f873
Bartelt Denny <denny.bartelt@mairdumont.com>
2019-10-02 12:14:33 +0200

    add test for parsing tcstring v1

--- src/tc-model-encoder.spec.ts
+++ src/tc-model-encoder.spec.ts
@@ -25,6 +25,22 @@ describe('TcModelEncoder', (): void => {
 
   describe('decode', (): void => {
 
+    it('should decode [Core] (Version 1, no purposes allowed)', (): void => {
+      const tc = 'BObdrPUOevsguAfDqFENCNAAAAAmeAAA';
+      const { decoded: tcModel } = encoder.decode(tc);
+      expect(tcModel).toBeDefined();
+      expect(tcModel.created.toISOString()).toBe('2019-02-04T21:16:05.200Z');
+      expect(tcModel.lastUpdated.toISOString()).toBe('2019-04-09T14:35:10.200Z');
+      expect(tcModel.cmpId).toBe(31);
+      expect(tcModel.cmpVersion).toBe(234);
+      expect(tcModel.consentScreen).toBe(5);
+      expect(tcModel.consentLanguage).toBe('EN');
+      expect(tcModel.vendorListVersion).toBe(141);
+      expect(tcModel.purposeConsents.toArray()).toStrictEqual([]);
+      // TODO maxVendorId = 615
+      expect(tcModel.vendorsAllowed.toArray()).toStrictEqual([]);
+    });
+
     it('should decode [Core]', (): void => {
       const tc = 'BObdrPUOevsguAfDqFENCNAAAAAmeAAA';
       const { decoded: tcModel } = encoder.decode(tc);
